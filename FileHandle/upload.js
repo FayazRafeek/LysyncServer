59fbc0bbb918f1c593707e59557ece5b055cd271
Fayaz Mohamed <fayazmrafeek@gmail.com>
2021-05-24 15:26:16 +0530

    Upload File test#1

---
+++ FileHandle/upload.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+const fileUpload = require('express-fileupload');
+const verifyToken = require('../Auth/VerifyToken');
+
+// default options
+router.use(fileUpload());
+router.use('/form', express.static(__dirname + '/index.html'));
+
+router.post('/uploadFile', function(req, res, next) {
+  let sampleFile;
+  let uploadPath;
+
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  sampleFile = req.files.sampleFile;
+  uploadPath = __dirname + ' + sampleFile.name';
+
+  console.log(sampleFile);
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(uploadPath, function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
+});
+
+module.exports = router;
+
+
--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var app = express();
 var AuthController = require('./Auth/AuthController');
 var apiRouter = require('./DataLogic/api');
 var deviceRout = require('./DataLogic/Device')
+var upload = require('./FileHandle/upload')
 
 app.use('/api/auth', AuthController);
 app.use('/api/data', apiRouter);
 app.use('/api/device', deviceRout);
+app.use('/api/upload', upload);
 
 module.exports = app;
